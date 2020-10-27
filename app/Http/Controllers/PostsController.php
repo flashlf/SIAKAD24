273834b6938416cf0348e605810a0b795c05a619
Flash Lavon <flash.lavon@hotmail.com>
2020-10-27 20:37:48 +0700

    Penggunaan Eloquent pada Laravel

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function show($param)
     {
-        $tmp = [
-            'first-post'    => "Hello ini post pertama dari param",
-            'second-post'   => "Ini post kedua dari param"
-        ];
-        if(! array_key_exists($param, $tmp)) abort(404,'Maaf, halaman tidak ditemukan');
+        //$tmp = \DB::table('post')->where('slug',$param)->first();
+        $tmp = Post::where('slug', $param)->firstOrFail();
+        if(! $tmp) abort(404);
+        //dd($tmp);
+        // $tmp = [
+        //     'first-post'    => "Hello ini post pertama dari param",
+        //     'second-post'   => "Ini post kedua dari param"
+        // ];
+        //if(! array_key_exists($param, $tmp)) abort(404,'Maaf, halaman tidak ditemukan');
         return view('post', [
-            'post' => $tmp[$param]
+            'post' => $tmp
         ]);
     }
 }
