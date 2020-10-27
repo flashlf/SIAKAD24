93914ba50fb1acaa880001d93c225e810a35c40f
Flash Lavon <flash.lavon@hotmail.com>
2020-10-27 19:22:27 +0700

    Add new view index.php and new routes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Informasi Akademi SMKN 24</title>
+</head>
+<body>
+    Ini adalah Halaman Index
+</body>
+</html>
