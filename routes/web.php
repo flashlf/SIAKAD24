130c2eeb29167ca7c0400aebdea722cf4a8ed898
flashlf <flash.lavon@hotmail.com>
2024-10-31 14:24:03 +0700

    feat: Re-initiate project using laravel 11

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,29 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $name = request('name');
-    return view('index', [
-        'name' => $name
-    ]);
-});
-
-Route::get('/about',function()  {
-    return view('about');
-});
-// Route::get('/post/{param}', function($param) {
-//     $tmp = [
-//         'first-post'    => "Hello ini post pertama dari param",
-//         'second-post'   => "Ini post kedua dari param"
-//     ];
-//     if(! array_key_exists($param, $tmp)) abort(404,'Maaf, halaman tidak ditemukan');
-//     return view('post', [
-//         'post' => $tmp[$param]
-//     ]);
-// });
-
-Route::get('/post/{param}', [PostsController::class, 'show']);
-
-Route::get('/welcome', function() {
     return view('welcome');
 });
-
